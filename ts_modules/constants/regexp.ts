3bb105507572e693e484224f7c3e430deeb86f55
837241830 <837241830@qq.com>
2017-09-27 18:55:29 +0800

    公共正则

--- ts_modules/constants/regexp.ts
+++ ts_modules/constants/regexp.ts
@@ -15,4 +15,14 @@ export class RegExgConstant {
 
     // 中文
     static chinese = new RegExp('[\\u4E00-\\u9FFF]+', 'g');
+
+    // 字母开头的数字或字母
+    static numberAlphabet = /^[a-zA-Z][0-9a-zA-Z]{0,}$/;
+
+    // 仅为数字且不以0开头
+    static positiveInteger = /^[1-9][0-9]*$/;
+
+    // 手机号验证
+    static phoneReg = /^1\d{10}$/g;
+
 }
